2e3aed02fe3dae270ce1db8e3dea990a8ebbfd1d
nouryashy <nourhanashmawy135@gmail.com>
2022-01-27 03:48:50 +0200

    nn

--- app/src/main/java/com/example/postandstreetresponsejava/APIInterface.java
+++ app/src/main/java/com/example/postandstreetresponsejava/APIInterface.java
@@ -7,12 +7,15 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface APIInterface {
+    //rxCall
 //    @GET("posts")
 //    public Single<List<PostModel>> getPosts();
 
+    //callBack call
 //    @GET("posts")
 //    public Call<List<PostModel>> getPosts();
 
+    //callBack call of street api
         @GET("streeters")
     public Call<List<PostModel>> getPosts();
 }
